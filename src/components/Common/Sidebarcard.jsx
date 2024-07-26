0f5651a2e9304139eed7c2618ec6f11edd1e62f4
debankanmitra <debankanmitra@gmail.com>
2024-07-26 11:56:19 +0530

    chat users

--- src/components/Common/Sidebarcard.jsx
+++ src/components/Common/Sidebarcard.jsx
@@ -1,4 +1,32 @@
 function Sidebarcard() {
+	const sortedUsers = [
+		{
+			img: "https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-01_pfck4u.jpg",
+			name: "Marie Zulfikar",
+			lastmessage: "The video chat ended · 2hrs",
+		},
+		{
+			img: "https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-02_vll8uv.jpg",
+			name: "Nhu Cassel",
+			lastmessage: "Hello Lauren 👋, · 24 Mar",
+		},
+		{
+			img: "https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-03_uzwykl.jpg",
+			name: "Patrick Friedman",
+			lastmessage: "Yes, you’re right but… · 14 Mar",
+		},
+		{
+			img: "https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-04_ttlftd.jpg",
+			name: "Byrne McKenzie",
+			lastmessage: "Hey Lauren ✨, first of all… · 14 Mar",
+		},
+		{
+			img: "https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-05_bktgmb.jpg",
+			name: "Scott Micheal",
+			lastmessage: "No way 🤙! · 11 Mar",
+		},
+	];
+
 	return (
 		// <!-- Snippet -->
 		<section className="flex min-h-screen flex-col justify-center bg-gray-50 p-4 text-gray-600 antialiased">
@@ -27,7 +55,8 @@ function Sidebarcard() {
 										<h2 className="text-xl font-bold leading-snug">
 											Lauren Marsano
 										</h2>
-									</a><br/>
+									</a>
+									<br />
 									<span className="inline-flex items-center gap-x-1.5 rounded-full bg-green-100 px-3 py-1 text-xs font-medium text-blue-800">
 										<span className="inline-block size-1.5 rounded-full bg-green-800"></span>
 										Online
@@ -79,113 +108,40 @@ function Sidebarcard() {
 						{/* <!-- Chat list --> */}
 						<div className="divide-y divide-gray-200">
 							{/* <!-- User --> */}
-							<button className="w-full py-2 text-left focus:outline-none focus-visible:bg-indigo-50">
-								<div className="flex items-center">
-									<img
-										className="mr-3 flex-shrink-0 items-start rounded-full"
-										src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-01_pfck4u.jpg"
-										width="32"
-										height="32"
-										alt="Marie Zulfikar"
-									/>
-									<div>
-										<h4 className="text-sm font-semibold text-gray-900">
-											Marie Zulfikar
-										</h4>
-										<div className="text-[13px]">
-											The video chat ended · 2hrs
+							{sortedUsers.map((user, index) => (
+								<button
+									key={index}
+									className="w-full py-2 text-left focus:outline-none focus-visible:bg-indigo-50"
+								>
+									<div className="flex items-center">
+										<img
+											className="mr-3 flex-shrink-0 items-start rounded-full"
+											src={user.img}
+											width="32"
+											height="32"
+											alt="Marie Zulfikar"
+										/>
+										<div>
+											<h4 className="text-sm font-semibold text-gray-900">
+												{user.name}
+											</h4>
+											<div className="text-[13px]">{user.lastmessage}</div>
 										</div>
 									</div>
-								</div>
-							</button>
-							{/* <!-- User --> */}
-							<button className="w-full py-2 text-left focus:outline-none focus-visible:bg-indigo-50">
-								<div className="flex items-center">
-									<img
-										className="mr-3 flex-shrink-0 items-start rounded-full"
-										src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-02_vll8uv.jpg"
-										width="32"
-										height="32"
-										alt="Nhu Cassel"
-									/>
-									<div>
-										<h4 className="text-sm font-semibold text-gray-900">
-											Nhu Cassel
-										</h4>
-										<div className="text-[13px]">Hello Lauren 👋, · 24 Mar</div>
-									</div>
-								</div>
-							</button>
-							{/* <!-- User --> */}
-							<button className="w-full py-2 text-left focus:outline-none focus-visible:bg-indigo-50">
-								<div className="flex items-center">
-									<img
-										className="mr-3 flex-shrink-0 items-start rounded-full"
-										src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-03_uzwykl.jpg"
-										width="32"
-										height="32"
-										alt="Patrick Friedman"
-									/>
-									<div>
-										<h4 className="text-sm font-semibold text-gray-900">
-											Patrick Friedman
-										</h4>
-										<div className="text-[13px]">
-											Yes, you’re right but… · 14 Mar
-										</div>
-									</div>
-								</div>
-							</button>
-							{/* <!-- User --> */}
-							<button className="w-full py-2 text-left focus:outline-none focus-visible:bg-indigo-50">
-								<div className="flex items-center">
-									<img
-										className="mr-3 flex-shrink-0 items-start rounded-full"
-										src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-04_ttlftd.jpg"
-										width="32"
-										height="32"
-										alt="Byrne McKenzie"
-									/>
-									<div>
-										<h4 className="text-sm font-semibold text-gray-900">
-											Byrne McKenzie
-										</h4>
-										<div className="text-[13px]">
-											Hey Lauren ✨, first of all… · 14 Mar
-										</div>
-									</div>
-								</div>
-							</button>
-							{/* <!-- User --> */}
-							<button className="w-full py-2 text-left focus:outline-none focus-visible:bg-indigo-50">
-								<div className="flex items-center">
-									<img
-										className="mr-3 flex-shrink-0 items-start rounded-full"
-										src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-05_bktgmb.jpg"
-										width="32"
-										height="32"
-										alt="Scott Micheal"
-									/>
-									<div>
-										<h4 className="text-sm font-semibold text-gray-900">
-											Scott Micheal
-										</h4>
-										<div className="text-[13px]">No way 🤙! · 11 Mar</div>
-									</div>
-								</div>
-							</button>
+								</button>
+							))}
 						</div>
+						{/* <!-- Bottom right button --> */}
+						<button className="absolute bottom-5 right-5 inline-flex items-center rounded-full bg-indigo-500 px-3 py-2 text-center text-sm font-medium text-white shadow-lg hover:bg-indigo-600 focus:outline-none focus-visible:ring-2">
+							<svg
+								className="mr-2 h-3 w-3 flex-shrink-0 fill-current text-indigo-300"
+								viewBox="0 0 12 12"
+							>
+								<path d="M11.866.146a.5.5 0 0 0-.437-.139c-.26.044-6.393 1.1-8.2 2.913a4.145 4.145 0 0 0-.617 5.062L.305 10.293a1 1 0 1 0 1.414 1.414L7.426 6l-2 3.923c.242.048.487.074.733.077a4.122 4.122 0 0 0 2.933-1.215c1.81-1.809 2.87-7.94 2.913-8.2a.5.5 0 0 0-.139-.439Z" />
+							</svg>
+							<span>New Chat</span>
+						</button>
 					</div>
-					{/* <!-- Bottom right button --> */}
-					<button className="absolute bottom-5 right-5 inline-flex items-center rounded-full bg-indigo-500 px-3 py-2 text-center text-sm font-medium text-white shadow-lg hover:bg-indigo-600 focus:outline-none focus-visible:ring-2">
-						<svg
-							className="mr-2 h-3 w-3 flex-shrink-0 fill-current text-indigo-300"
-							viewBox="0 0 12 12"
-						>
-							<path d="M11.866.146a.5.5 0 0 0-.437-.139c-.26.044-6.393 1.1-8.2 2.913a4.145 4.145 0 0 0-.617 5.062L.305 10.293a1 1 0 1 0 1.414 1.414L7.426 6l-2 3.923c.242.048.487.074.733.077a4.122 4.122 0 0 0 2.933-1.215c1.81-1.809 2.87-7.94 2.913-8.2a.5.5 0 0 0-.139-.439Z" />
-						</svg>
-						<span>New Chat</span>
-					</button>
 				</div>
 			</div>
 		</section>
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,4 +1,4 @@
-import { Avatar } from "@mui/material";
+// import { Avatar } from "@mui/material";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext.jsx";
 import PropTypes from "prop-types";
@@ -11,29 +11,43 @@ const Users = ({ users, onUserClick, selectedUser }) => {
 	});
 
 	return (
-		<div className="overflow-y-auto h-[calc(100vh-128px)] p-2">
-			{sortedUsers.map((user) => {
-				if (user.uid !== currentUser.uid) {
-					return (
-						<div
-							className={`${
-								user.uid === selectedUser?.uid ? "bg-slate-200" : ""
-							} flex items-center gap-2 p-3 cursor-pointer rounded-lg`}
-							key={user.uid}
-							onClick={() => onUserClick(user)}
-						>
-							{user.photoURL ? (
-								<Avatar src={user.photoURL} />
-							) : (
-								<Avatar>{user.displayName?.[0]}</Avatar>
-							)}
-							<div>
-								<h4>{user.displayName}</h4>
+		<div className="px-5 py-3">
+			<h3 className="mb-1 text-xs font-semibold uppercase text-gray-400">
+				Chats
+			</h3>
+			<div className="overflow-y-auto h-[calc(100vh-128px)] p-2 divide-y divide-gray-200">
+				{sortedUsers.map((user) => {
+					if (user.uid !== currentUser.uid) {
+						return (
+							<div
+								className={`${
+									user.uid === selectedUser?.uid ? "bg-slate-200" : ""
+								} flex gap-2 p-3 cursor-pointer rounded-lg`}
+								key={user.uid}
+								onClick={() => onUserClick(user)}
+							>
+								<div className="flex items-center">
+									<img
+										className="w-10 h-10 rounded-full mr-3 flex-shrink-0 items-start"
+										src={user.photoURL}
+										width="46"
+										height="46"
+										alt={user.displayName}
+									/>
+									<div>
+										<h4 className="text-sm font-semibold text-gray-900">
+											{user.displayName}
+										</h4>
+										<div className="text-[13px]">
+											Hey Lauren ✨, first of all… · 14 Mar
+										</div>
+									</div>
+								</div>
 							</div>
-						</div>
-					);
-				}
-			})}
+						);
+					}
+				})}
+			</div>
 		</div>
 	);
 };
