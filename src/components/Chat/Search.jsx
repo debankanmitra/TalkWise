1662b5be3f00a26566c21cba70690c4698ee49e7
debankanmitra <debankanmitra@gmail.com>
2024-07-26 12:04:44 +0530

    search

--- src/components/Chat/Search.jsx
+++ src/components/Chat/Search.jsx
@@ -5,11 +5,11 @@ import PropTypes from "prop-types";
 const Search = ({ searchKeyword, onSearchChange }) => {
 	return (
 		<div className="search border-b border-slate-200 h-16 flex">
-			<div className="flex items-center gap-2 px-4 text-sm py-2 m-2 rounded-lg bg-slate-100 w-full">
+			<div className="flex items-center gap-2 px-4 text-sm py-2 m-2 bg-gray-100 focus-within:shadow-lg w-full rounded-xl">
 				<SearchIcon className="w-20 h-auto text-slate-500" />
 				<input
 					type="text"
-					className="bg-inherit w-full"
+					className="h-full w-full bg-gray-100 pr-2 text-sm text-gray-700 outline-none"
 					placeholder="Search user..."
 					onChange={(e) => onSearchChange(e.target.value)}
 					value={searchKeyword}
